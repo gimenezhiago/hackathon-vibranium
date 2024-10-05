4b3c0edcad0d4cb0c6b3c875452f3bf8d3febf6a
D4fto <163424876+D4fto@users.noreply.github.com>
2024-10-05 20:18:32 -0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
-const path = require('path');
+const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars');
 app.use(express.static('public'))
 
@@ -8,11 +9,25 @@ app.engine('handlebars', exphbs.engine({defaultLayout:'default'}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+async function busca(lat, long) {
+    console.log('fouhf')
+    const respone = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${lat},${long}&apikey=LAMwoHLn1OwUgSGuAfX7oo9QBxRFVATF`)
+    const data = await respone.json()
+    return await data.timelines.hourly
+}
 app.get('/', (req, res) => {
     res.render('home')
 });
 app.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+    busca(req.query.lat,req.query.long).then((result) => {
+        res.render('dashboard',{data: result})
+        
+    }).catch((err) => {
+        
+    });
 });
 app.get('/feedback', (req, res) => {
     res.render('feedback')
@@ -21,10 +36,6 @@ app.get('/map', (req, res) => {
     res.render('map')
 });
 
-// async function busca(lat, long){
-//     const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${lat},${long}&apikey=EbMZ5fZpYQLR45NDGzcPkPlkwHUOkEgg`)
-//     const result = await 
-// }
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
